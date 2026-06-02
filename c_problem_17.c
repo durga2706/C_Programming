185f246b1fa37b379d808bc69ee64d7b9ca4b3a9
durga2706 <durkadevi0610@gmail.com>
2026-06-02 18:49:04 +0530

    Create c_problem_17.c

---
+++ c_problem_17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, result;
+    printf("Enter a two digit number:");
+    scanf("%d", &a);
+    output=(a/10)*10;
+    printf("%d",output);
+    return 0;
+}
